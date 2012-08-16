5bba70f152d9d53627c6d77c181537959d00f4aa
unknown <carlos.rodriguez@Sandslash.masterit.local>
2012-08-16 12:32:06 -0300

    Alteracao com sucesso

--- Carlos/src/executaveis/Fibonacci.java
+++ Carlos/src/executaveis/Fibonacci.java
@@ -3,24 +3,24 @@ package executaveis;
 import java.util.Scanner;
 
 public class Fibonacci {
-//	private static Scanner scan;
-//	private static int[] fibonacci;
-//	private static int val;
-//
-//	public static void main(String[] args) {
-//		System.out.println("Informe para quantos números deseja realizar" 
-//				+ " a equação de fibonacci");
-//		scan = new Scanner(System.in);
-//		int num = scan.nextInt(); 
-//		fibonacci = new int[num + 1];
-//		
-//		fibonacci[0] = 0;
-//		fibonacci[1] = 1;
-//		
-//		for(int i = 2; i <= num; i++) {
-//			val = fibonacci[i - 2] + fibonacci[i - 1];
-//			fibonacci[i] = val;
-//			System.out.println(fibonacci[i]);
-//		}
-//	}
+	private static Scanner scan;
+	private static int[] fibonacci;
+	private static int val;
+
+	public static void main(String[] args) {
+		System.out.println("Informe para quantos números deseja realizar" 
+				+ " a equação de fibonacci");
+		scan = new Scanner(System.in);
+		int num = scan.nextInt(); 
+		fibonacci = new int[num + 1];
+		
+		fibonacci[0] = 0;
+		fibonacci[1] = 1;
+		
+		for(int i = 2; i <= num; i++) {
+			val = fibonacci[i - 2] + fibonacci[i - 1];
+			fibonacci[i] = val;
+			System.out.println(fibonacci[i]);
+		}
+	}
 }
